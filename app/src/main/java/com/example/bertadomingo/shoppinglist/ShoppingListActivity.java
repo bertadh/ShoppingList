82a0fbe421afbad51d4ebaac85c627595cf562fb
berta.domingo <bertadomingo1@hotmail.com>
2017-10-20 16:04:43 +0200

    Vesió amb el métode addItem

--- app/src/main/java/com/example/bertadomingo/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/bertadomingo/shoppinglist/ShoppingListActivity.java
@@ -2,11 +2,14 @@ package com.example.bertadomingo.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,6 +39,32 @@ public class ShoppingListActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ItemList);
 
         shopping_list.setAdapter(adapter);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+            }
+        });
+
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
+
+
+    }
+
+    private void addItem() {
+        String txt = edit_item.getText().toString();
+        if(!txt.isEmpty()){
+            ItemList.add(txt);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
     }
 
 }
