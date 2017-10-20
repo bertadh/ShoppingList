2dd8ee668cbb45e899a87b1987a8fd7db77ce1bd
berta.domingo <bertadomingo1@hotmail.com>
2017-10-20 15:38:36 +0200

    Vesió amb el layout

--- app/src/main/java/com/example/bertadomingo/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/bertadomingo/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,29 @@ package com.example.bertadomingo.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ShoppingListActivity extends AppCompatActivity {
 
+
+    private ListView shopping_list;
+    private Button btn_add;
+    private EditText edit_item;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        shopping_list = (ListView) findViewById(R.id.shopping_list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
     }
+
 }
